9afc62ed05c8ffc98ee71f4245f3304295d9cb99
Sha980 <142073864+Sha980@users.noreply.github.com>
2023-08-12 10:27:43 +0530

    Shashank.jha
    I am a student of class 12th 
    I know web development
    And now I am learning c++,python,java

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 shashank jha web developer and mechanical engineer I’m @Sha980
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaboration
+best technique I have to improve the web
+rajanand jha my father 
+<!---raman chatrawas student 
+Sha980/Sha980 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
